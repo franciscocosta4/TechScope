bdf5c36792fe7edbc9c0efd2731596efaf979ea6
Francisco da silva costa <80585387+franciscocosta4@users.noreply.github.com>
2026-07-24 15:23:52 +0100

    primeira versão com playright do scrapper para indeed.com

--- indeed.py
+++ indeed.py
@@ -1,35 +1,59 @@
-import requests
+from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 
 url = "https://www.indeed.com/jobs?q=python&l=Texas"
-headers = {
-    "User-Agent": "Mozilla/5.0",
-    "Accept-Language": "en-US,en;q=0.9",
-}
 
-resp = requests.get(url, headers=headers, timeout=20)
-# resp.raise_for_status()
-print(resp.status_code)
-print(resp.text[:1000])
+with sync_playwright() as p:
 
-soup = BeautifulSoup(resp.text, "html.parser")
+    # Inicia o Chromium
+    browser = p.chromium.launch(
+        headless=False  # False para testar
+    )
+
+    # Cria um contexto semelhante a um navegador real
+    context = browser.new_context(
+        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
+        locale="en-US"
+    )
+
+    page = context.new_page()
+
+    # Abre a página
+    page.goto(url, wait_until="networkidle")
+
+    # Obtém o HTML já renderizado
+    html = page.content()
+
+    browser.close()
+
+soup = BeautifulSoup(html, "html.parser")
 
 results = soup.find(id="mosaic-jobResults")
-job_cards = results.find_all("td", class_="resultContent css-1o6lhys eu4oa1w0")
 
-for job_card in job_cards:
-    title = card.select_one("[class*='jcs-JobTitle']")
-    # company = card.select_one("[class*='_subtitle']")
-    location = card.select_one("[class*='company_location']")
-    link = card.select_one("[class*='jcs-JobTitle']")
+if results is None:
+    print("Não foi encontrado 'mosaic-jobResults'.")
+    exit()
 
-    if title and company:
-        print(
-            title.get_text(strip=True),
-            "|",
-            # company.get_text(strip=True),
-            # "|",
-            location.get_text(strip=True) if location else "",
-            "|",
-            link["href"] if link and link.has_attr("href") else ""
-        )
+job_cards = results.select("[data-testid='slider_item']")
+
+print(f"Foram encontrados {len(job_cards)} anúncios.\n")
+
+for card in job_cards:
+
+    title = card.select_one("[data-testid='job-title']")
+
+    company = card.select_one("[data-testid='company-name']")
+
+    location = card.select_one("[data-testid='text-location']")
+
+    link = card.select_one("a")
+
+    print(
+        title.get_text(strip=True) if title else "",
+        "|",
+        company.get_text(strip=True) if company else "",
+        "|",
+        location.get_text(strip=True) if location else "",
+        "|",
+        "https://www.indeed.com" + link["href"] if link and link.has_attr("href") else ""
+    )
