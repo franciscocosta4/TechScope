460b737dba37aa0c0a15efce5216131cfa290064
Francisco da silva costa <80585387+franciscocosta4@users.noreply.github.com>
2026-09-01 22:34:33 +0100

    agora o path do .bat é dinamico

--- start_chrome_debug.bat
+++ start_chrome_debug.bat
@@ -1,5 +1,5 @@
 @echo off
 set CHROME_PATH=C:\Program Files (x86)\Google\Chrome\Application\chrome.exe
-set PROFILE_DIR=C:\Users\Utilizador\CODE\TechScope\data-pipeline\scrapers\chrome-profile
+set PROFILE_DIR=%~dp0chrome-profile
 
 "%CHROME_PATH%" --remote-debugging-port=9222 --user-data-dir="%PROFILE_DIR%"
