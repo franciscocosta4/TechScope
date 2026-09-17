0a61f4a1a878831ee7858ac7be877ccdbb45b554
Francisco da silva costa <80585387+franciscocosta4@users.noreply.github.com>
2026-09-17 09:57:33 +0100

    deixamos de usar a landing page dentro do .net para passar a usar a do github pages

--- web-app/Program.cs
+++ web-app/Program.cs
@@ -36,6 +36,6 @@ app.UseRouting();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
 app.Run();
